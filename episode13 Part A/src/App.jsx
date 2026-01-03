e9bff5e1cd3a3f3972e4083c68f7bbb43c3d83df
Priyanshu <Preyanshukandari@gmail.com>
2026-01-03 23:48:28 +0530

    Episode 13 Part A completed

---
+++ episode13 Part A/src/App.jsx
@@ -0,0 +1,19 @@
+import { Person } from './person'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Person 
+      name = {9}
+      email = "preyanshukandari@gmail.com"
+      age = {18}
+      isMarried = {true}
+      friends = {["Kunal","Raman" , "Jatin"]}
+      />
+    </>
+  )
+}
+
+export default App
